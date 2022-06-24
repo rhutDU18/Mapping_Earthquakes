45473e692446d6801954d6ce785c57741da60f04
Robert Hutchins <rhutchins747@gmail.com>
2022-06-24 13:15:31 -0600

    Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,3 +1,6 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,24 +23,16 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom:3,
-  layers: [streets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 });
 
-
-
-L.controllayers(baseMaps).addTo(map);
-
+// Pass our map layers into our layer control and add the layer control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data,{
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
-    }
-})
-.addTo(map);
+  L.geoJson(data).addTo(map);
 });

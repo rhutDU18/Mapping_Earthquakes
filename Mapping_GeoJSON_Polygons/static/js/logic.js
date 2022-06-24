dc5194e954b13014f16de2763629f45c33a91607
Robert Hutchins <rhutchins747@gmail.com>
2022-06-23 20:23:32 -0600

    Mapping_GeoJSON_Polygons

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -4,9 +4,10 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     maxZoom: 18,
     accessToken: API_KEY
 });
-// We create the dark view tile layer that will be an option for our map.
+
+// We create the tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
@@ -33,8 +34,9 @@ let airportData = "https://raw.githubusercontent.com/RhutDU18/Mapping_Earthquake
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/rhutDU18/Mapping_Earthquakes/Mapping_GeoJSON_Points/torontoRoutes.json";
 
+
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+let torontoHoods = "https://mmetwalli.s3.us-east-2.amazonaws.com/torontoNeighborhoods.json";
 
 
 // Create a style for the lines.
@@ -43,17 +45,14 @@ let myStyle = {
     weight: 2
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-
-L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-        + feature.properties.dst + "</h3>");
-    }
- })
- .addTo(map);
-});
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data,{
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+        }
+    })
+    .addTo(map);
+    });

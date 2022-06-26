1fb14178192451cc1748f57dae3afab802ae716f
Robert Hutchins <rhutchins747@gmail.com>
2022-06-25 18:45:05 -0600

    Mapping_Earthquakes_past7days

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -23,10 +23,6 @@ let satelliteStreets = L.tileLayer(
   }
 );
 
-
-
-
-
 // Create the map object with center, zoom level and default layer.
 let map = L.map("mapid", {
   center: [39.5, -98.5],
@@ -43,18 +39,7 @@ d3.json(
   // the map. We pass the magnitude of the earthquake into a function
   // to calculate the radius.
 
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: "#ffae42",
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5,
-    };
-  }
-  // This function determines the radius of the earthquake marker based on its magnitude.
+// This function determines the radius of the earthquake marker based on its magnitude.
   // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
   function getRadius(magnitude) {
     if (magnitude === 0) {
@@ -62,38 +47,6 @@ d3.json(
     }
     return magnitude * 4;
   }
-
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function (feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-        },
-    // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // We create a popup for each circleMarker to display the magnitude and
-    //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup(
-        "Magnitude: " +
-          feature.properties.mag +
-          "<br>Location: " +
-          feature.properties.place
-      );
-    },
-  }).addTo(earthquakes);
-
-    // Then we add the earthquake layer to our map.
-    earthquakes.addTo(map);
-
-
-
-
-legend.addTo(map);
-
-    
-    
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -121,8 +74,6 @@ legend.addTo(map);
     }
     return "#98ee00";
   }
-
-
   // Create a base layer that holds both maps.
   let baseMaps = {
     Streets: streets,
@@ -141,5 +92,60 @@ legend.addTo(map);
   // which layers are visible.
   L.control.layers(baseMaps, overlays).addTo(map);
 
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+        },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup(
+        "Magnitude: " +
+          feature.properties.mag +
+          "<br>Location: " +
+          feature.properties.place
+      );
+    },
 
-});
+  }).addTo(earthquakes);
+
+
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+  
+  });
+  
+  // Then add all the details for the legend
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+  
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+  
+  // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+  
+    // Finally, we our legend to the map.
+    legend.addTo(map);
+
+})
